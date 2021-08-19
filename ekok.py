d24e032b4c5320e1635fb73beda3e4c60220c79b
ozgeksuz <ozge-oksuz1@hotmail.com>
2021-08-19 15:21:24 +0300

    Update ekok.py

--- ekok.py
+++ ekok.py
@@ -2,7 +2,7 @@ def ekok(sayi1,sayi2):
     #en büyük ekok değeri ikisinin çarpımıdır.
     ekok = sayi1*sayi2
 
-    for i in range (ekok, max(sayi1,sayi2),-1):
+    for i in range (ekok, max(sayi1,sayi2)-1,-1):
         if i % sayi1 == 0 and i % sayi2 == 0:
             ekok = i
     return ekok
