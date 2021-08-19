0eed855f32cfc66043173917f5401db8dd788c3a
ozgeksuz <ozge-oksuz1@hotmail.com>
2021-08-19 15:19:24 +0300

    ekok.py

---
+++ ekok.py
@@ -0,0 +1,13 @@
+def ekok(sayi1,sayi2):
+    #en büyük ekok değeri ikisinin çarpımıdır.
+    ekok = sayi1*sayi2
+
+    for i in range (ekok, max(sayi1,sayi2),-1):
+        if i % sayi1 == 0 and i % sayi2 == 0:
+            ekok = i
+    return ekok
+
+birinciSayi = int(input("Birinci sayiyi giriniz: "))
+ikinciSayi = int(input("ikinciSayi sayiyi giriniz: "))
+
+print(ekok(birinciSayi,ikinciSayi))
